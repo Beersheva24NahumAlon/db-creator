992a60278a431ce3d7f26317461e2fffcc9d9d13
Nahum Alon <nochum.shepsl@gmail.com>
2024-12-27 17:36:25 +0300

    Used conditions from homework

--- src/main/java/telral/employees/Main.java
+++ src/main/java/telral/employees/Main.java
@@ -1,15 +1,16 @@
 package telral.employees;
 
+import static telral.employees.Properties.*;
 import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
         String fileName = "employees-sql-data.csv";
         DepartmentStaff[] departments = {
-                new DepartmentStaff("QA", 1, 1, 4, 0),
-                new DepartmentStaff("Development", 1, 3, 3, 0),
-                new DepartmentStaff("Devops", 1, 4, 2, 0),
-                new DepartmentStaff("Sales", 1, 0, 0, 5)
+                new DepartmentStaff("QA", QA_N_MANAGERS, QA_N_EMPLOYEES, QA_N_WAGE_EMPLOYEES, QA_N_SALES_PERSONS),
+                new DepartmentStaff("Development", DEV_N_MANAGERS, DEV_N_EMPLOYEES, DEV_N_WAGE_EMPLOYEES, DEV_N_SALES_PERSONS),
+                new DepartmentStaff("Devops", DEVOPS_N_MANAGERS, DEVOPS_N_EMPLOYEES, DEVOPS_N_WAGE_EMPLOYEES, DEVOPS_N_SALES_PERSONS),
+                new DepartmentStaff("Sales", SALES_N_MANAGERS, SALES_N_EMPLOYEES, SALES_N_WAGE_EMPLOYEES, SALES_N_SALES_PERSONS)
         };
         try (PrintWriter writer = new PrintWriter(fileName)) {
             writeFile(writer, departments);
@@ -20,11 +21,11 @@ public class Main {
 
     private static void writeFile(PrintWriter writer, DepartmentStaff[] departments) {
         for (int i = 0; i < departments.length; i++) {
-            for (int e = 0; e < departments[i].nEmployees(); e++) {
-                writer.println(Employee.getEmployee(departments[i].department()));
-            }
             for (int e = 0; e < departments[i].nManagers(); e++) {
                 writer.println(Employee.getManager(departments[i].department()));
+            }
+            for (int e = 0; e < departments[i].nEmployees(); e++) {
+                writer.println(Employee.getEmployee(departments[i].department()));
             }
             for (int e = 0; e < departments[i].nWageEmployees(); e++) {
                 writer.println(Employee.getWageEmployee(departments[i].department()));
